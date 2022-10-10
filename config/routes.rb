dbe9ed78dd7d309555292b24919668fdb8db6a64
Navid Seyedashrafi <navidalexander@Navids-MacBook-Pro.local>
2022-10-10 12:25:45 -0700

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'main#home'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
